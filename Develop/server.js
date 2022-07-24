01ac6188d2b6ce72e635efa9c79f3abf6b092267
Jeffrey <wzhang@wzhangs-MacBook-Pro.local>
2022-07-24 14:14:50 -0700

    added get route

---
+++ Develop/server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const path = require('path');
+// Helper method for generating unique ids
+const uuid = require('uuid');
+const notes = require('./db/db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+// GET request for reviews
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+
+  console.log(notes);
+  res.json(notes);
+  
+});
+
+// GET request for a single review
+// GET - /api/reviews/13
+// app.get('/api/reviews/:review_id', (req, res) => {
+//   console.log(req.query.list)
+//   console.log("req.body", req.body);
+//   console.log("req.params.review_id", req.params.review_id);
+//   if (req.body && req.params.review_id) {
+//     console.info(`${req.method} request received to get a single a review`);
+//     const reviewId = req.params.review_id;
+//     for (let i = 0; i < reviewList.length; i++) {
+//       const currentReview = reviewList[i];
+//       if (currentReview.review_id === reviewId) {
+//         res.json(currentReview);
+//         return;
+//       }
+//     }
+//     res.json('Review ID not found');
+//   }
+// });
+
+// POST request to add a review
+// app.post('/api/notes', (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to add a review`);
+
+//   // Destructuring assignment for the items in req.body
+//   const { id, title, text } = req.body;
+  
+//   if (title && text) {
+ 
+//     const newNote = {
+//       // req.body.title,
+//       text,
+//       id: uuid(),
+//     };
+//     notes.push(newNote);
+
+//     const response = {
+//       status: 'success',
+//       body: newNote,
+//     };
+
+//     console.log(response);
+//     res.json(response);
+//   } else {
+//     res.json('Error in posting review');
+//   }
+// });
+
+// // GET request for upvotes
+// app.get('/api/upvotes', (req, res) => {
+//   // Inform the client
+//   res.json(`${req.method} request received to retrieve upvote count`);
+
+//   // Log our request to the terminal
+//   console.info(`${req.method} request received to retrieve upvote count`);
+// });
+
+// Post request to upvote a review
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
